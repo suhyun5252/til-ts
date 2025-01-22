9be6ac943b8e67389881498fd746dfe5c42435b1
suhyun5252 <o52o.suhyun@gmail.com>
2025-01-22 09:58:44 +0900

    [docs] 기본형 타입 호환성

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
-// 비동기 함수
-async function fetchGetTodo(): Promise<void> {
-  const res = await fetch("주소");
-}
+// 리터럴은 실제 값을 말한다.
+// 아래는 "hello" 는 "hello"라는 리터럴이다.
 
-async function fetchGetTodoOne(): Promise<string> {
-  const res = await fetch("주소");
-  return "hi";
-}
+const constStr: "hello" = "hello";
+// const 상수로 만들면 값은 "hello"로 고정이 됩니다.
+// 그러나 어찌되었던지 "hello" string
 
-async function fetchPostTodoOne(): Promise<boolean> {
-  const res = await fetch("주소");
-  return true;
-}
+// "hello" 리터럴은 문자열에 포함된다. (업캐스팅 된다.)
+let str: string = "constStr";
 
-type Todo = {
-  id: number;
-  title: string;
-};
-async function fetchSortTodoOne(): Promise<Todo> {
-  const res = await fetch("주소");
-  return { id: 1, title: "안녕" };
-}
+// 리터럴로 표현하면
+let num: 100 = 100;
+//  100 리터럴은 숫자형에 포함된다. (업케스팅 된다.)
+let num2: number = num;
+
+// false 리터럴은 boolean 에 포함된다. (업케스팅 된다.)
+const isLive: boolean = false;
