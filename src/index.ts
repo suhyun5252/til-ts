993b97991f2818380fb8305546717785e346a61c
suhyun5252 <o52o.suhyun@gmail.com>
2025-02-12 09:55:13 +0900

    generic 기초

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
-// 약속을 지켜라
-interface CarInterface {
-  name: string;
-  brand: string;
-  price: number;
-  stop(): void;
-  move(): void;
-}
-interface ElectricInterface {
-  baterry: number;
-  isBaterry: boolean;
-}
-// 인터페이스를 구현
-class ElectricCar implements CarInterface, ElectricInterface {
-  constructor(
-    public name: string,
-    public brand: string,
-    public price: number,
-    public baterry: number,
-    public isBaterry: boolean // 1. public 추가 2. 철자 수정
-  ) {}
-  stop() {
-    console.log("멈춰");
-  }
-  move() {
-    console.log("움직여");
-  }
+// 함수 리턴타입에서 리턴종류를 모르면
+// any 또는 unknow 을 리턴한다.
+// 만약 원하는 것이 있다면 정확히 작성해야 한다.
+
+function showValue<T>(value: T): T {
+  return value;
 }
 
-let 자동차 = new ElectricCar("캐스퍼", "현대", 1000, 10, true);
-자동차.stop();
-자동차.move();
+let test = showValue(10);
+let test2 = showValue("안녕");
+let test3 = showValue(true);
+let test4 = showValue([1, 2, 3]);
